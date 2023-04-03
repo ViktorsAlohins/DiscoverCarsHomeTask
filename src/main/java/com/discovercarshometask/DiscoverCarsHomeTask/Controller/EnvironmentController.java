fa16ff0d5f50d3eeebadbfb159f081f41258bfd1
ViktorsAlohins <spyxed@gmail.com>
2023-04-03 21:49:52 +0300

    Making background and foreground colors in HTML change based on values passed to variables bgColor and fgColor. Values are case in-sensitive and only accept hex code.

--- src/main/java/com/discovercarshometask/DiscoverCarsHomeTask/Controller/EnvironmentController.java
+++ src/main/java/com/discovercarshometask/DiscoverCarsHomeTask/Controller/EnvironmentController.java
@@ -1,9 +1,5 @@
 package com.discovercarshometask.DiscoverCarsHomeTask.Controller;
 
-<<<<<<< HEAD
-import com.discovercarshometask.DiscoverCarsHomeTask.Model.MessageWrapper;
-=======
->>>>>>> a5aa195 (Creating simple application that makes HTTP request and returns "Hello World" in 3 different formats - HTTP, Json or XML)
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
@@ -12,25 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
-import java.util.regex.Pattern;
-
-@RestController
-public class EnvironmentController {
-
-    private static final Pattern HEX_COLOR_PATTERN = Pattern.compile("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$",
-            Pattern.CASE_INSENSITIVE);
-
-    @GetMapping("/api/environment")
-    public ResponseEntity<String> getHelloWorld(@RequestParam(value = "format", required = false, defaultValue = "html")
-                                                String format) {
-=======
 @RestController
 public class EnvironmentController {
 
     @GetMapping("/api/environment")
     public ResponseEntity<String> getHelloWorld(@RequestParam(value = "format", required = false, defaultValue = "html") String format) {
->>>>>>> a5aa195 (Creating simple application that makes HTTP request and returns "Hello World" in 3 different formats - HTTP, Json or XML)
         String message = "Hello World";
 
         switch (format.toLowerCase()) {
@@ -44,22 +26,7 @@ public class EnvironmentController {
     }
 
     private ResponseEntity<String> generateHtmlOutput(String message) {
-<<<<<<< HEAD
-        String bgColor = System.getenv().getOrDefault("BGCOLOR", "#FFFFFF");
-        String fgColor = System.getenv().getOrDefault("FGCOLOR", "#000");
-
-        if (!HEX_COLOR_PATTERN.matcher(bgColor).matches()) {
-            bgColor = "#FFFFFF";
-        }
-
-        if (!HEX_COLOR_PATTERN.matcher(fgColor).matches()) {
-            fgColor = "#000000";
-        }
-
-        String html = String.format("<html><head><style>body { background-color: %s; color: %s; }</style></head><body>%s</body></html>", bgColor, fgColor, message);
-=======
         String html = String.format("<html><body>%s</body></html>", message);
->>>>>>> a5aa195 (Creating simple application that makes HTTP request and returns "Hello World" in 3 different formats - HTTP, Json or XML)
         return ResponseEntity.ok().contentType(MediaType.TEXT_HTML).body(html);
     }
 
@@ -78,9 +45,6 @@ public class EnvironmentController {
         return ResponseEntity.ok().contentType(MediaType.APPLICATION_XML).body(xmlOutput);
     }
 
-<<<<<<< HEAD
-}
-=======
     private static class MessageWrapper {
         private String message;
 
@@ -97,4 +61,3 @@ public class EnvironmentController {
         }
     }
 }
->>>>>>> a5aa195 (Creating simple application that makes HTTP request and returns "Hello World" in 3 different formats - HTTP, Json or XML)
