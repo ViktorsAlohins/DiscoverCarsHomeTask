a133f0d686ab903db3f4726b0a8cfcd9c889942f
viktors.alohins <spyxed@gmail.com>
2023-04-11 15:20:12 +0300

    Adding two variables bgColor and fgColor, now you can change colors not only in code, but also when making a /environment HTML request use this - localhost:8080/api/environment?format=html&bgColor=%23"COLORCODE"&fgColor=%23"COLORCODE". Please note "%23" before color code, not "#".

--- src/main/java/com/discovercars/hometask/DiscoverCarsHomeTask/controller/EnvironmentController.java
+++ src/main/java/com/discovercars/hometask/DiscoverCarsHomeTask/controller/EnvironmentController.java
@@ -9,11 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.regex.Pattern;
+
 @RestController
 public class EnvironmentController {
 
+    private static final Pattern HEX_COLOR_PATTERN = Pattern.compile("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$",
+            Pattern.CASE_INSENSITIVE);
+
     @GetMapping("/api/environment")
-    public ResponseEntity<String> getHelloWorld(@RequestParam(value = "format", required = false, defaultValue = "html") String format) {
+    public ResponseEntity<String> getHelloWorld(
+            @RequestParam(value = "format", required = false, defaultValue = "html") String format,
+            @RequestParam(value = "bgColor", required = false, defaultValue = "#FFFFFF") String bgColor,
+            @RequestParam(value = "fgColor", required = false, defaultValue = "#000000") String fgColor) {
+
         String message = "Hello World";
 
         switch (format.toLowerCase()) {
@@ -22,12 +31,21 @@ public class EnvironmentController {
             case "xml":
                 return generateXmlOutput(message);
             default:
-                return generateHtmlOutput(message);
+                return generateHtmlOutput(message, bgColor, fgColor);
         }
     }
 
-    private ResponseEntity<String> generateHtmlOutput(String message) {
-        String html = String.format("<html> %s", message);
+    private ResponseEntity<String> generateHtmlOutput(String message, String bgColor, String fgColor) {
+
+        if (!HEX_COLOR_PATTERN.matcher(bgColor).matches()) {
+            bgColor = "#FFFFFF";
+        }
+
+        if (!HEX_COLOR_PATTERN.matcher(fgColor).matches()) {
+            fgColor = "#000000";
+        }
+
+        String html = String.format("<html><head><style>body { background-color: %s; color: %s; }</style></head><body>%s</body></html>", bgColor, fgColor, message);
         return ResponseEntity.ok().contentType(MediaType.TEXT_HTML).body(html);
     }
 
